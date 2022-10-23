c60406dc1f731bedb38c9b1683ed7132a0082e42
EnriqueFeola <feolanemec@gmail.com>
2022-10-23 15:35:45 -0300

    Portfolio web

---
+++ react-tailwindcss-portfolio-master/src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Bootcamp realizado en Senpai Academy de Desarrollador Web Full Stack. El curso fue muy completo y me permitió aprender a desarrollar aplicaciones web desde cero. El profesor es muy atento y resuelve todas las dudas que se le presentan a los alumnos.",
+    image:
+      "https://senpaiacademy.com/wp-content/uploads/2021/12/Pleno_Imagotipo-e1638919187684-1024x790.webp",
+    name: "Desarrollador Web Full Stack",
+    company: "Senpai Academy",
+  },
+  {
+    quote:
+      "Estudios universitarios en la Universidad de la Republica. Curse y termine la carrera de Abogacia. Me permitió aprender a tratar con diferenets tipos de clientes y trabaje en equipo con colegas, lo que me permitio tener una mayor seguridad al trabajar en equipo.",
+    image:
+      "https://www.erasmus-care.eu/_wp/wp-content/uploads/2017/05/Logo_Udelar_300x233-1.jpg",
+    name: "Abogado",
+    company: "Universidad de la Republica",
+  },
+];
+
+export const skills = ["JavaScript", "React", "Next.js", "Node", "PostgresSQL"];
